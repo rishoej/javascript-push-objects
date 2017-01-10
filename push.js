a08c6ebbb28492aa21c085f5ed1d09bb89d9dabf
Tobias Rishøj <rishoej@users.noreply.github.com>
2017-01-10 08:37:13 +0100

    update

---
+++ push.js
@@ -0,0 +1,9 @@
+var price = 200;
+var name = 'product name';
+var element = {};
+var products = [];
+
+element.product_name = name;
+element.product_price = price;
+products.push(element);
+console.log(products);
